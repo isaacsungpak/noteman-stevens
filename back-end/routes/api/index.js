27e70ada85f12f080fceb8d597fc6900d59cc1c0
isaacsungpak <sungyulpak@gmail.com>
2021-12-05 03:23:59 -0500

    logout

--- back-end/routes/api/index.js
+++ back-end/routes/api/index.js
@@ -35,7 +35,6 @@ const sessionRouter = require('./session.js');
 const usersRouter = require('./users.js');
 
 router.use('/session', sessionRouter);
-
 router.use('/users', usersRouter);
 
 router.post('/test', (req, res) => {
