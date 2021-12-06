dc326462c431aa2803701febe059a454924f9d19
isaacsungpak <sungyulpak@gmail.com>
2021-12-05 20:31:06 -0500

    prep for heroku deployment

--- back-end/routes/index.js
+++ back-end/routes/index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-
 const apiRouter = require('./api');
 
 router.use('/api', apiRouter);
@@ -30,10 +29,5 @@ if (process.env.NODE_ENV !== 'production') {
     return res.json({});
   });
 }
-
-router.get('/hello/world', function(req, res) {
-  res.cookie('XSRF-TOKEN', req.csrfToken());
-  res.send('Hello World!');
-});
 
 module.exports = router;
