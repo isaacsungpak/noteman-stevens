72face87377b618465d432a873f9ae938221f6c2
isaacsungpak <sungyulpak@gmail.com>
2021-12-08 10:24:38 -0500

    notepad partition

--- frontend/src/components/NotesPage/index.js
+++ frontend/src/components/NotesPage/index.js
@@ -29,46 +29,51 @@ function NotesPage() {
     return (
         <>
             {!user && <Redirect to="/" />}
-            <div id='notes-page-top-bar'>
-                <div id='notes-header'>Notes</div>
-                <form>
-                    <input onChange={(e) => setNoteName(e.target.value)} value={noteName} type='text' placeholder='Searchman' />
-                </form>
-            </div>
+            <div id="note-page-container">
+                <div id="notes-collection-container">
+                    <div id='notes-page-top-bar'>
+                        <div id='notes-header'>Notes</div>
+                        <form>
+                            <input onChange={(e) => setNoteName(e.target.value)} value={noteName} type='text' placeholder='Searchman' />
+                        </form>
+                    </div>
 
-            <div className='note-grid-top'>
-                <p id='note-count'>{`${notes.length ? notes.length : 0} notes`}</p>
-                {/* <button className="new-note-btn" onClick={() => setShowCreateModal(true)}>
-                    New Note
-                </button> */}
-                {/* {showCreateModal && <CreateModal notebooks={notebooks} setShowCreateModal={setShowCreateModal}/>} */}
-            </div>
+                    <div className='note-grid-top'>
+                        <p id='note-count'>{`${notes.length ? notes.length : 0} notes`}</p>
+                        {/* <button className="new-note-btn" onClick={() => setShowCreateModal(true)}>
+                            New Note
+                        </button> */}
+                        {/* {showCreateModal && <CreateModal notebooks={notebooks} setShowCreateModal={setShowCreateModal}/>} */}
+                    </div>
 
-            <div className='note-grid'>
-                <table>
-                    <thead>
-                        <tr className="odd-row">
-                            <th className="note-title">TITLE</th>
-                            <th className="note-author">LOCATED IN</th>
-                            <th className="note-created">CREATED ON</th>
-                            <th className="note-updated">UPDATED</th>
-                            <th className="note-actions">ACTIONS</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {visibleNotes.length > 0 && visibleNotes.map((note, i) =>
-                                <tr key={i} className={i % 2 === 0 ? "even-row" : "odd-row"}>
-                                    <td className="note-title">{note.title}</td>
-                                    <td className="note-author">{note.Notebook.title}</td>
-                                    <td className="note-created">{`${note.createdAt.slice(5,7)}/${note.createdAt.slice(8,10)}/${note.createdAt.slice(0,4)}`}</td>
-                                    <td className="note-updated">{`${note.updatedAt.slice(5,7)}/${note.updatedAt.slice(8,10)}/${note.updatedAt.slice(0,4)}`}</td>
-                                    {/* <td className="note-actions">
-                                        <ActionsButton note={note}/>
-                                    </td> */}
+                    <div className='note-grid'>
+                        <table>
+                            <thead>
+                                <tr className="odd-row">
+                                    <th className="note-title">TITLE</th>
+                                    <th className="note-author">LOCATED IN</th>
+                                    <th className="note-created">CREATED ON</th>
+                                    <th className="note-updated">UPDATED</th>
+                                    <th className="note-actions">ACTIONS</th>
                                 </tr>
-                        )}
-                    </tbody>
-                </table>
+                            </thead>
+                            <tbody>
+                                {/* {visibleNotes.length > 0 && visibleNotes.map((note, i) =>
+                                        <tr key={i} className={i % 2 === 0 ? "even-row" : "odd-row"}>
+                                            <td className="note-title">{note.title}</td>
+                                            <td className="note-author">{note.Notebook.title}</td>
+                                            <td className="note-created">{`${note.createdAt.slice(5,7)}/${note.createdAt.slice(8,10)}/${note.createdAt.slice(0,4)}`}</td>
+                                            <td className="note-updated">{`${note.updatedAt.slice(5,7)}/${note.updatedAt.slice(8,10)}/${note.updatedAt.slice(0,4)}`}</td>
+                                        </tr>
+                                )} */}
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+                <div id="notepad-container">
+                    <input type="text" placeholder="Note title"/>
+                    <textarea placeholder="Note content"></textarea>
+                </div>
             </div>
         </>
     )
