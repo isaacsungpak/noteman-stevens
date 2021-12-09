b5afa6e41ede3061be43c212359161121f71dfb1
isaacsungpak <sungyulpak@gmail.com>
2021-12-09 10:08:19 -0500

    special surprise ;)

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useLocation } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
 import './Navigation.css';
@@ -16,13 +17,35 @@ function Navigation({ isLoaded }) {
         </div>
     );
 
+    const [specialLogo, setSpecialLogo] = useState(false);
+    /////////////////////////// easter egg
+    const location = useLocation();
+    const path = location.pathname;
+    const [count, setCount] = useState(0);
+
+    const logoClick = () => {
+      if (path === '/') setCount(count + 1);
+    }
+
+    useEffect(() => {
+      if (count === 7) setSpecialLogo(true);
+    }, [count, specialLogo])
+
+    useEffect(() => {
+      let threeSecCount = setInterval((() => setCount(0)), 3000);
+
+      return clearInterval(threeSecCount);
+    })
+    ///////////////////////////
+
+
     return (
       <div id="nav-div">
         <ul id="nav-bar">
           {sessionUser && <NavLink to="/notebooks">Notebooks</NavLink>}
           <li id="logo-link-container">
             <NavLink exact to="/" id="logo-link">
-              <img src='/noteman-stevens-logo.png' id="ns-logo" alt="logo" />
+              <img src={specialLogo ? '/notemanx3.png' : '/noteman-stevens-logo.png'} id="ns-logo" alt="logo" disabled={path === '/'} onClick={logoClick} />
             </NavLink>
           </li>
             {isLoaded && sessionLinks}
